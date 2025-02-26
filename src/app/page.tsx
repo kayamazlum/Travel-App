a6b9776ad3e0d7a5bbfd505007552796bf0cb684
kayamazlum <mazlum09kaya@gmail.com>
2025-02-26 20:55:16 +0300

    hero bolumu tamalamdi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="text-5xl">
+    <div className="">
       <Home />
     </div>
   );
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
-  return <div className="overflow-hidden h-[10000px]">Home</div>;
+  return (
+    <div className="overflow-hidden ">
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
