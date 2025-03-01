59b8e28d3dacef28b8d97205cd0b6dc78abd9672
kayamazlum <mazlum09kaya@gmail.com>
2025-03-02 00:06:15 +0300

    why choose us bolumu olsuturldu

---
+++ src/components/Home/WhyChoose/WhyChooseCard.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  image: string;
+  title: string;
+};
+
+const WhyChooseCard = ({ image, title }: Props) => {
+  return (
+    <div>
+      {/* IMAGE */}
+      <Image
+        src={image}
+        width={70}
+        height={70}
+        alt="image"
+        className="mx-auto"
+      />
+      {/* CONTENT */}
+      <h1 className="mt-6 text-center text-gray-900 font-medium text-lg">
+        {title}
+      </h1>
+      <p className="mt-2 text-center text-xs font-medium text-gray-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis error
+        illo id eligendi.
+      </p>
+    </div>
+  );
+};
+
+export default WhyChooseCard;
