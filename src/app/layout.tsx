ef6c48c97ac26aef7feb2950ef047a93d9c5c9f6
kayamazlum <mazlum09kaya@gmail.com>
2025-03-04 18:14:25 +0300

    scrolltotop comp olusturuldu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollTotop from "@/components/Helper/ScrollTotop";
 
 const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -36,6 +37,7 @@ export default function RootLayout({
         <ResponsiveNav />
         {children}
         <Footer />
+        <ScrollTotop />
       </body>
     </html>
   );
