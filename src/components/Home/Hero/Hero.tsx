c10dbac84383a9183cfe3a9891ac1184fd693696
kayamazlum <mazlum09kaya@gmail.com>
2025-03-04 19:12:10 +0300

    efektler eklendi ve proje bitti

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
       {/* TEXT CONTENT */}
       <div className="absolute z-[100] w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <div className="flex items-center justify-center flex-col w-full h-full">
-          <div>
+          <div data-aos="fade-up">
             <h1 className="text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white font-bold uppercase">
               Lets Enjoy The Nature
             </h1>
--- src/components/Home/News/News.tsx
+++ src/components/Home/News/News.tsx
@@ -8,28 +8,40 @@ const News = () => {
       {/* SECTION HEADING */}
       <SectionHeading heading="Exciting Travel News For You" />
       <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20">
-        <div>
+        <div data-aos="fade-left" data-aos-anchor-placement="top-center">
           <NewsCard
             image="/images/n1.jpg"
             title="Top 10 place to visit in Australia"
             date="15 November 2024"
           />
         </div>
-        <div>
+        <div
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="100"
+        >
           <NewsCard
             image="/images/n2.jpg"
             title="Top 10 place to visit in Germany"
             date="25 November 2024"
           />
         </div>
-        <div>
+        <div
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="200"
+        >
           <NewsCard
             image="/images/n3.jpg"
             title="Top 10 place to visit in England"
             date="22 November 2024"
           />
         </div>
-        <div>
+        <div
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="300"
+        >
           <NewsCard
             image="/images/n4.jpg"
             title="Top 10 place to visit in New Zealand"
